170ff04ddd785f4e864ea55e482906d7571b6e52
Shubham Jha <shubham.jha2001m@gmail.com>
2021-02-05 22:32:49 +0530

    MongoDB Connection

--- server.js
+++ server.js
@@ -16,3 +16,15 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server Running on Port ${PORT}`);
 });
+
+// MongoDB Connection
+const URI = process.env.MONGO_URI;
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw err;
+    console.log('MongoDB Connected ...');
+})
